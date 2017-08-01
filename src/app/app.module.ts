b0ee088a9698bb01c7afdcf4f1bbf4ecff9e2b2b
dawlab <dawidlabno@outlook.com>
2017-08-01 20:30:00 +0200

    Dodanie stron aplikacji: badania, badanie, edytuj-badanie, lokalizuj, tabs

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+/*Zaimportowanie stron aplikacji*/
+import { MyApp } from './app.component';
+import { BadaniaPage } from '../pages/badania/badania';
+import { BadaniePage } from '../pages/badanie/badanie';
+import { EdytujBadaniePage } from '../pages/edytuj-badanie/edytuj-badanie';
+import { LokalizujPage } from '../pages/lokalizuj/lokalizuj';
+import { TabsPage } from '../pages/tabs/tabs';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    BadaniaPage,
+    BadaniePage,
+    EdytujBadaniePage,
+    LokalizujPage,
+    TabsPage
+    
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    BadaniaPage,
+    BadaniePage,
+    EdytujBadaniePage,
+    LokalizujPage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
